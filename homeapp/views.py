ca7357741c3e8b3a61eafd688c7cac12161245f2
molasolo <314815080@qq.com>
2022-09-21 13:34:05 +0800

    已完成home模板静态资源配置

---
+++ homeapp/views.py
@@ -0,0 +1,9 @@
+from cgitb import html
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+def home(request):
+    # html = '<html><body>首页</body></html>'
+    # return HttpResponse(html)
+    return render(request,'home.html')
