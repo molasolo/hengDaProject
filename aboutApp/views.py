fb4920ed5f2d1d738659c5aea8bd6ef1360d138c
molasolo <314815080@qq.com>
2022-09-23 17:13:30 +0800

    数据库模型建立

--- aboutApp/views.py
+++ aboutApp/views.py
@@ -6,9 +6,13 @@ from django.shortcuts import HttpResponse
 def survey(request):
     # html = '<html><body>企业概况</body></html>'
     # return HttpResponse(html)
-    return render(request, 'survey.html', {'active_menu': 'about',})
+    return render(request, 'survey.html', 
+        {'active_menu': 'about', 'sub_menu': 'survey', })
+
 
 def honor(request):
     # html = '<html><body>荣誉资质</body></html>'
     # return HttpResponse(html)
-    return render(request, 'honor.html', {'active_menu': 'about'})
+    return render(request, 'honor.html',  
+        {'active_menu': 'about', 'sub_menu': 'honor', })
+          
--- hengDaProject/settings.py
+++ hengDaProject/settings.py
@@ -134,3 +134,5 @@ import os
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
