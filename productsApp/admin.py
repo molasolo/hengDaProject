a8fcd5f45e7ebbeb63e62ed410f1d59d62d8e829
molasolo <314815080@qq.com>
2022-09-28 17:44:58 +0800

    分页显示

--- productsApp/admin.py
+++ productsApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product,ProductImg
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductImg)
--- productsApp/models.py
+++ productsApp/models.py
@@ -1,3 +1,7 @@
+from email.policy import default
+from itertools import product
+from tabnanny import verbose
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.utils import timezone
 
@@ -19,3 +23,28 @@ class Product(models.Model):
                                 null=True,
                                 verbose_name='产品价格'
                                 )
+    publishDate = models.DateTimeField(max_length=20,
+                                        default=timezone.now,
+                                        verbose_name='发布时间')
+    views = models.PositiveIntegerField('浏览量', default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = '产品'
+        verbose_name_plural = '产品'
+        ordering = ('-publishDate',)
+
+class ProductImg(models.Model):
+    product = models.ForeignKey(Product,
+                                related_name = 'productImgs',
+                                verbose_name = '产品',
+                                on_delete = models.CASCADE)
+    photo = models.ImageField(upload_to = 'Product/',
+                                blank = True,
+                                verbose_name = '产品图片')
+    
+    class Meta:
+        verbose_name = '产品图片'
+        verbose_name_plural = '产品图片'
